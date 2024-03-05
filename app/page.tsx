18049ded11b3c8ce3b46ab9711e8fbb5a617616a
Sebastian Rodriguez <sebasstian.rdz@gmail.com>
2024-03-05 17:11:12 -0600

    some fix up

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import { withPageAuthRequired, getSession } from "@auth0/nextjs-auth0";
-
-async function Home() {
+export default async function Home() {
   return (
     <div className="bg-muted h-screen w-full flex justify-center items-center">
       <a href="/api/auth/login">Login</a>
     </div>
   );
 }
-
-export default withPageAuthRequired(Home, { returnTo: "/dashboard" });
