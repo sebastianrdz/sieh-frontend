937216fec5ae55ebabe5cdf06aa05dee4aa9911a
Sebastian Rodriguez <sebasstian.rdz@gmail.com>
2024-03-08 21:14:29 -0600

    login fix

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,11 +1,7 @@
 import Login from "@/components/Login";
 import React from "react";
 
-interface Props {
-  className?: string;
-}
-
-function SigniInPage(propps: Props) {
+function SigniInPage() {
   return <Login />;
 }
 
